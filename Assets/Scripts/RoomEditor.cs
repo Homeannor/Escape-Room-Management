c1a005079f80f7c90c53d05132ff082a0032f5f2
abig006@icloud.com <abig006@icloud.com>
2025-10-24 13:30:02 +0100

    Added a basic Main Menu and Room Editor Panel

---
+++ Assets/Scripts/RoomEditor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RoomEditor : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup Panel;
+
+    public void OpenClosePanel()
+    {
+        Panel.alpha = Panel.alpha > 0 ? 0 : 1;
+        Panel.interactable = Panel.interactable == true ? false : true; //this makes the panel only be interactable when it's visible so the buttons don't get clicked when the panel is closed
+    }
+
+    public void TestButtons()
+    {
+        Debug.Log("Button Pressed");
+    }
+
+}
