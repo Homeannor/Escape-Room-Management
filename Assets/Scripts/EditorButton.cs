a5282f4bead4216b9732638aa19fe5c8c2c24266
Abi Gillespie <AG1637@student.aru.ac.uk>
2025-12-01 10:06:14 +0000

    Fixed UI clicking issue + added Preview Image & Cancel Building

--- Assets/Scripts/EditorButton.cs
+++ Assets/Scripts/EditorButton.cs
@@ -1,22 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EditorButton : MonoBehaviour
 {
     private RoomEditor roomEditor;
     private GameObject itemPrefabs;
+    public GameObject previewImage;
     Buildmanager Bm;
 
+    [System.Obsolete]
     void Start()
     {
         Bm = Buildmanager.instance;
         roomEditor = FindObjectOfType<RoomEditor>();
         itemPrefabs = GameObject.Find("== ITEM PREFABS ==");
 
+        previewImage = GameObject.FindWithTag("PreviewImage");
+        //previewImage.SetActive(false);
+
         Debug.Log("Current Item: " + itemPrefabs.transform.Find(gameObject.name));
     }
 
     public void RoomEditorButton()
     {
+        previewImage.SetActive(true);
+        previewImage.GetComponent<Image>().sprite = gameObject.GetComponent<Image>().sprite;
         Debug.Log("Item Selected: " + itemPrefabs.transform.Find(gameObject.name));
         Bm.SetBuildingTobuild(itemPrefabs.transform.Find(gameObject.name).gameObject);
         roomEditor.OpenClosePanel();
