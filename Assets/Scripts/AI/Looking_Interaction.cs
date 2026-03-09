339485c225d07245511e7fa556f12f2f144b6ecf
ag1637 <ag1637@student.aru.ac.uk>
2026-03-09 15:16:12 +0000

    Added scripts and box colliders to every item

--- Assets/Scripts/AI/Looking_Interaction.cs
+++ Assets/Scripts/AI/Looking_Interaction.cs
@@ -1,18 +1,40 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Looking_Interaction : MonoBehaviour
 {
     private int reactionTime;
+    public bool alreadyLooked = false;
+    private int randomNumber;
+    public int successRate; //used to make some props less obvious clues (lower number means the customer is less likely to stop and look at it)
 
     private void OnTriggerEnter(Collider other)
     {
-        reactionTime = Random.Range(1, 10); //waits for a random amount of time
+        reactionTime = Random.Range(1, 10);
+        randomNumber = Random.Range(1, 100);
         if (other.CompareTag("Customer"))
         {
-            Debug.Log("Interacting with" + gameObject.name);
-            other.GetComponent<AIMovement>().StopMoving();
-            other.GetComponent<AIMovement>().Invoke("StartMoving", reactionTime);
+            if (randomNumber < successRate)
+            {
+                if (alreadyLooked == false)
+                {
+                    Debug.Log("Interacting with" + gameObject.name);
+                    other.GetComponent<AIMovement>().StopMoving();
+                    other.GetComponent<AIMovement>().Invoke("StartMoving", reactionTime);
+                    alreadyLooked = true;
+                }
+                else
+                {
+                    StartCoroutine(ChangeBoolean());
+                }
+            }
         }
+    }
+
+    private IEnumerator ChangeBoolean()
+    {
+        yield return new WaitForSeconds(30);
+        alreadyLooked = false;
     }
 }
